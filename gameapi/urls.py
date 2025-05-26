4c37e7c268137c526ae683738dbe3adc0e445e07
Chang Leroy <105347195+mecho10@users.noreply.github.com>
2025-05-26 06:27:10 -0500

    Update urls.py

--- gameapi/urls.py
+++ gameapi/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('', RedirectView.as_view(url='/static/snake/index.html')),
 ]
